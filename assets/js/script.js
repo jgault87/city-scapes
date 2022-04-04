27a2eb4e7bbe6ab2b51a4a5da5d77e97ccaec382
Natalya Colburn <greatqueenbee@gmail.com>
2022-04-03 18:23:03 -0600

    adjusted size of results card and added a footer

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+var userFormEl = document.querySelector('#user-form');
+var getCityEl = document.querySelector('#city-name');
+var button = document.querySelector('.btn');
+var img = document.querySelector('#image');
+
+
+userFormEl.addEventListener("submit", function(event) {
+    event.preventDefault();
+    getCoordinates(getCityEl.value);
+
+
+    var url = "https://api.teleport.org/api/urban_areas/slug:" + getCityEl.value.toLowerCase() + "/images/";
+
+    fetch(url)
+        .then(function(response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+
+                    img.src = data.photos[0].image.web;
+                })
+            }
+        })
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    M.Materialbox.init(elems);
+  });
