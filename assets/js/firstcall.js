1b742b3089c0182610e848b7bc3b80b816802c0d
jgault87 <99287734+jgault87@users.noreply.github.com>
2022-04-06 14:52:01 -0600

    parsed city/state country to page for search, set up wiki ID URL for additional city info, added function to clear inputted city upon submission

--- assets/js/firstcall.js
+++ assets/js/firstcall.js
@@ -1,17 +1,11 @@
 var APIKey = "f3ffd1bebd9a1cb69ed3311049f6cdca";
 
-
-
-
 function getCoordinates(city) {
-    
   callURL =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     city +
     "&appid=" +
     APIKey;
-
-  
 
   fetch(callURL)
     .then(function (response) {
@@ -22,32 +16,62 @@ function getCoordinates(city) {
       return response.json();
     })
 
-    
     .then(function (response) {
       let CoordLat = response.coord.lat;
       let CoordLon = response.coord.lon;
       console.log(response);
 
-
       const options = {
-        method: 'GET',
+        method: "GET",
         headers: {
-            'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
-            'X-RapidAPI-Key': 'e5e2b6f887msh0ddb6a2c600bdc3p1762f9jsn6415dabc6973'
-            
-        }
+          "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+          "X-RapidAPI-Key":
+            "e5e2b6f887msh0ddb6a2c600bdc3p1762f9jsn6415dabc6973",
+        },
+      };
 
-    };
+      fetch(
+        "https://wft-geo-db.p.rapidapi.com/v1/geo/cities?location=" +
+          CoordLat +
+          CoordLon +
+          "&minPopulation=25000&distanceUnit=MI",
+        options
+      )
+        // .then(response => response.json())
+        // .then(response => console.log(response.data[0].wikiDataId));
+        // .catch(err => console.error(err));
 
-    
-    fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/cities?location='+ CoordLat + CoordLon + '&minPopulation=25000&distanceUnit=MI', options)
-    
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+        .then(function (response) {
+          if (!response.ok) {
+            throw response.json();
+          }
+
+          return response.json();
+        })
+        .then(function (response) {
+          console.log(response);
+          
+          let city = response.data[0].city;
+          let state = response.data[0].region;
+          let country = response.data[0].country;
+
+          var place = city + ', ' + state + ' ' + '(' + country + ')';
+          console.log(place);
+          cardHeadEl.innerHTML = place;
+
+
+          let population = response.data[0].population;
+          let wikiData = response.data[0].wikiDataId;
+
+          let factsURL = "https://www.wikidata.org/wiki/" + wikiData;
+
+          console.log(factsURL);
+
+
+        });
     });
 }
-
+var cardHeadEl = document.getElementById('nametitle');
 
 var previousSearches = [];
 
@@ -62,13 +86,10 @@ function renderHistory() {
     li.textContent = previousSearch;
     li.setAttribute("data-index", i);
     li.addEventListener("click", function () {
-      //  searchReturn = li.textContent.split('clear');
+     
       searchReturn = this.textContent.split("clear");
-      console.log(searchReturn);
-      getCoordinates(searchReturn);
-      
-
-    
+      console.log(searchReturn[0]);
+      // getCoordinates(searchReturn);
 
       //  pass to function for search
     });
@@ -98,7 +119,6 @@ function storeHistory() {
   localStorage.setItem("previousSearches", JSON.stringify(previousSearches));
 }
 
-
 historyEl.addEventListener("click", function (event) {
   var element = event.target;
   // Checks if element is a button
@@ -111,5 +131,12 @@ historyEl.addEventListener("click", function (event) {
   }
 });
 
-
 init();
+
+
+
+function resetInput() {
+  getCityEl.value = "";
+
+  
+}
