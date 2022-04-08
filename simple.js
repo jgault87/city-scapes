c47a24c1227dba31b26afb0152f2741f7eec4b03
Ellen Furness <elasfern@gmail.com>
2022-04-08 17:21:53 -0600

    4-8-script-working-need-parse

--- simple.js
+++ simple.js
@@ -1,21 +1,21 @@
 
-  var getCityEl = document.querySelector('#city-name');
-  var modalButton = document.querySelector('.modal');
+var getCityEl = document.querySelector('#city-name');
+var modalButton = document.querySelector('.modal');
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems);
+});
 
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Host': 'community-citybikes.p.rapidapi.com',
-      'X-RapidAPI-Key': 'f17db1dcddmsh3dce12092ca5ebep17ef72jsn4fcd0fdb0eeb'
-    }
-  };
-  
-  fetch('https://community-citybikes.p.rapidapi.com/networks.json', options)
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'community-citybikes.p.rapidapi.com',
+    'X-RapidAPI-Key': 'f17db1dcddmsh3dce12092ca5ebep17ef72jsn4fcd0fdb0eeb'
+  }
+};
+
+fetch('https://community-citybikes.p.rapidapi.com/networks.json', options)
   // city +
   //   "name" +
   //   "idx" +
@@ -25,25 +25,24 @@
   //   "coordinates" +
   //   "&lat=&lng" +
   //   "station_url")
-    
-    .then(response => response.json())
-    .then(response => {
-      console.log(response)
-      if (response.ok) {
-        // log response
-        console.log(response);
-        response.json().then(function (data) {
-            // long data returned from server
-            console.log(data);
+
+  .then(response => response.json())
+  .then(response => {
+    console.log(response)
+    if (response.ok) {
+      // log response
+      console.log(response);
+      response.json().then(function (data) {
+        // long data returned from server
+        console.log(data);
         var factEl = document.getElementById("fact");
         factEl.textContent = data[0].city;
-        })
-      } 
-     })
-    .catch(err => console.error(err)); 
-      
-    
-  
-    
+      })
+    }
+  })
+  .catch(err => console.error(err));
 
-  
+
+
+
+
